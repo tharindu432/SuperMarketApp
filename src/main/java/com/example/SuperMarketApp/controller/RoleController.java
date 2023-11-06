7b8b394cac01909f7265af3fc1946639887c5215
tharindu432 <tharinduchaturanga432@gmail.com>
2023-11-06 15:24:28 +0530

    new role added to the database

--- src/main/java/com/example/SuperMarketApp/controller/RoleController.java
+++ src/main/java/com/example/SuperMarketApp/controller/RoleController.java
@@ -2,14 +2,20 @@ package com.example.SuperMarketApp.controller;
 
 import com.example.SuperMarketApp.entity.Role;
 import com.example.SuperMarketApp.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RoleController {
-    private RoleService roleService;
+    private final RoleService roleService;
+    @Autowired
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
     @PostMapping({"/createNewRole"})
-    public Role createNewRole(Role role){
+    public Role createNewRole( @RequestBody  Role role){
         return roleService.createNewRole(role);
 
     }
