3f58ce2a73b49ca394dc907c5f2220fc6b569ded
tharindu432 <tharinduchaturanga432@gmail.com>
2023-11-07 00:23:46 +0530

    cors configuration class created

--- src/main/java/com/example/SuperMarketApp/controller/UserController.java
+++ src/main/java/com/example/SuperMarketApp/controller/UserController.java
@@ -3,14 +3,22 @@ package com.example.SuperMarketApp.controller;
 import com.example.SuperMarketApp.entity.User;
 import com.example.SuperMarketApp.service.RoleService;
 import com.example.SuperMarketApp.service.UserService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
     @Autowired
     private final UserService userService;
+
+    @PostConstruct
+   public void initRolesAndUser(){
+    userService.initRolesAndUser();
+   }
     public UserController(UserService userService) {
         this.userService = userService;
     }
@@ -18,8 +26,20 @@ public class UserController {
 
 
     @PostMapping({"registerNewUser"})
-    public User registerNewUser(User user){
+    public User registerNewUser(@RequestBody User user){
       return   userService.registerNewUser(user);
 
     }
+
+    @GetMapping({"/forAdmin"})
+    public String forAdmin(){
+        return "this url is only accssible to admin";
+    }
+
+    @GetMapping({"/forUser"})
+    public String forUser(){
+        return "this url is only accssible to users";
+    }
+
+
 }
--- src/main/java/com/example/SuperMarketApp/service/UserService.java
+++ src/main/java/com/example/SuperMarketApp/service/UserService.java
@@ -1,17 +1,57 @@
 package com.example.SuperMarketApp.service;
 
+import com.example.SuperMarketApp.dao.RoleDao;
 import com.example.SuperMarketApp.dao.UserDao;
+import com.example.SuperMarketApp.entity.Role;
 import com.example.SuperMarketApp.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class UserService {
     @Autowired
     private UserDao userDao;
+
+    @Autowired
+    private RoleDao roleDao;
     public User registerNewUser(User user){
         return userDao.save(user);
 
 
+    }
+
+    public void initRolesAndUser(){
+        Role adminRole = new Role();
+        adminRole.setRoleName("Admin");
+        adminRole.setRoleDescription("Admin role");
+        roleDao.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setRoleName("User");
+        userRole.setRoleDescription("Default role for newly created record");
+        roleDao.save(userRole);
+
+        User adminUser = new User();
+        adminUser.setUserName("admin123");
+        adminUser.setUserPassword("admin@pass");
+        adminUser.setUserFirstName("admin");
+        adminUser.setUserLastName("admin");
+        Set<Role> adminRoles = new HashSet<>();
+        adminRoles.add(adminRole);
+        adminUser.setRole(adminRoles);
+        userDao.save(adminUser);
+
+        User user = new User();
+        user.setUserName("raj123");
+        user.setUserPassword("raj@123");
+        user.setUserFirstName("raj");
+        user.setUserLastName("sharma");
+        Set<Role> userRoles = new HashSet<>();
+        userRoles.add(userRole);
+        user.setRole(userRoles);
+        userDao.save(user);
     }
 }
