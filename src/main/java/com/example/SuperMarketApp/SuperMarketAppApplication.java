ea400fd7dbd14c231e2006a10ea403190b1d39ea
tharindu432 <tharinduchaturanga432@gmail.com>
2023-11-06 12:34:02 +0530

    initial commit

---
+++ src/main/java/com/example/SuperMarketApp/SuperMarketAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.SuperMarketApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperMarketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperMarketAppApplication.class, args);
+		System.out.println("Super market application");
+	}
+
+}
---
+++ src/test/java/com/example/SuperMarketApp/SuperMarketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SuperMarketApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperMarketAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
