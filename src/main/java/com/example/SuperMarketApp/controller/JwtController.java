2cc806c17be646626ed6e819ff7ba959116f6f28
tharindu432 <tharinduchaturanga432@gmail.com>
2023-11-07 19:11:16 +0530

    JwtController and JwtService Created

---
+++ src/main/java/com/example/SuperMarketApp/controller/JwtController.java
@@ -0,0 +1,24 @@
+package com.example.SuperMarketApp.controller;
+
+import com.example.SuperMarketApp.entity.JwtRequest;
+import com.example.SuperMarketApp.entity.JwtResponse;
+import com.example.SuperMarketApp.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class JwtController {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @PostMapping({"/authenticate"})
+    public JwtResponse createJwtToken(@RequestBody JwtRequest jwtRequest) throws Exception {
+        return jwtService.createJwtToken(jwtRequest);
+    }
+}
+
