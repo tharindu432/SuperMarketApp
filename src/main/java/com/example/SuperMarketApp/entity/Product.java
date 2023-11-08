bce0ebe850524e19a607f53ce4938fef0709d2d6
tharindu432 <tharinduchaturanga432@gmail.com>
2023-11-08 23:56:30 +0530

    relationshi between image model and product is completed

--- src/main/java/com/example/SuperMarketApp/entity/Product.java
+++ src/main/java/com/example/SuperMarketApp/entity/Product.java
@@ -1,9 +1,6 @@
 package com.example.SuperMarketApp.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Set;
 
 @Entity
@@ -17,7 +14,17 @@ public class Product {
     private Double productDiscountedPrice;
     private Double productActualPrice;
 
-    private Set<ImageModel>
+    @ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @JoinTable(name="product_images",
+    joinColumns = {
+            @JoinColumn(name = "product_id"),
+
+    },
+            inverseJoinColumns = {
+            @JoinColumn(name = "image_id")
+            }
+    )
+    private Set<ImageModel> productImages;
 
     public Integer getProductId() {
         return productId;
