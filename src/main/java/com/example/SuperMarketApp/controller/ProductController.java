95380e5633fe411f08b49ca61609a7c0e5720d5c
tharindu432 <tharinduchaturanga432@gmail.com>
2023-11-08 23:20:58 +0530

    product controller completed

--- src/main/java/com/example/SuperMarketApp/controller/ProductController.java
+++ src/main/java/com/example/SuperMarketApp/controller/ProductController.java
@@ -1,14 +1,19 @@
 package com.example.SuperMarketApp.controller;
 
 import com.example.SuperMarketApp.entity.Product;
+import com.example.SuperMarketApp.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
+    @Autowired
+    private ProductService productService;
 
     @PostMapping({"/addNewProduct"})
-    public void addNewProduct(Product product){
+    public Product addNewProduct(Product product){
+       return productService.addNewProduct(product);
 
 
     }
