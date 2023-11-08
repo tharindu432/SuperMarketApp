4e87cf5e062f5f143684824699125c1aab474aff
tharindu432 <tharinduchaturanga432@gmail.com>
2023-11-08 23:09:33 +0530

    ProductDao added

---
+++ src/main/java/com/example/SuperMarketApp/dao/ProductDao.java
@@ -0,0 +1,10 @@
+package com.example.SuperMarketApp.dao;
+
+import com.example.SuperMarketApp.entity.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductDao extends CrudRepository<Product,Integer> {
+
+}
