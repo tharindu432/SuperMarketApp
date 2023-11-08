fed5bdf0a758438fd5a26b5f8a906c215cb64b7e
tharindu432 <tharinduchaturanga432@gmail.com>
2023-11-08 23:19:18 +0530

    product Service added

---
+++ src/main/java/com/example/SuperMarketApp/service/ProductService.java
@@ -0,0 +1,17 @@
+package com.example.SuperMarketApp.service;
+
+import com.example.SuperMarketApp.dao.ProductDao;
+import com.example.SuperMarketApp.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductDao productDao;
+
+    public Product addNewProduct(Product product){
+       return productDao.save(product);
+    }
+}
